7fde4d66f3de42549ee8e2c1a11f11a58639a042
RatulKoley <ratuljob657@gmail.com>
2023-04-28 10:09:16 +0530

    Add project files.

---
+++ InfrastructureClassLibraryMethods/RegisterServices.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InfrastructureClassLibraryMethods
+{
+    internal static class RegisterServices
+    {
+        public static void ConfigureStructure(this IServiceCollection services, IConfiguration con)
+        {
+
+        }
+    }
+}
---
+++ UserRegistrationWithWebApiJWT/user.cs
@@ -0,0 +1,8 @@
+﻿namespace UserRegistrationWithWebApiJWT
+{
+    public class user
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
